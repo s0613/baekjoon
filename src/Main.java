5e5ed9b92846145ce256b4384e90f089945670c8
SongSeungJu <wkwjsrj5079@gmail.com>
2024-02-29 23:06:32 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int sum = 0;
-        for(int i = a; i <=b; i++){
-            if(i%c == 0){
-                sum += i;
-            }
-        }
-        System.out.println(sum);
+        System.out.println(1);
+        System.out.println(0);
     }
 }
